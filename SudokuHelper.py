1bdab5ad2f02788cb94c74986a47b208600970ca
Gabriel Mesquita <90516048+Gabriel-Mesq@users.noreply.github.com>
2022-07-16 07:59:49 -0300

    Automatização!

--- SudokuHelper.py
+++ SudokuHelper.py
@@ -1,4 +1,8 @@
 import numpy as np
+import pyautogui as pg
+import time
+
+start_time = time.time()
 
 sdk = [[0,0,0, 0,0,0, 0,0,0],
        [0,0,0, 0,0,0, 0,0,0],
@@ -11,6 +15,44 @@ sdk = [[0,0,0, 0,0,0, 0,0,0],
        [0,0,0, 0,0,0, 0,0,0],
        [0,0,0, 0,0,0, 0,0,0],
        [0,0,0, 0,0,0, 0,0,0]]
+
+size = 56
+
+for r in range(0,9):
+    for c in range(0, 9):
+        
+        if pg.locateOnScreen('0.png', grayscale=True, region=(363+56*c, 238+56*r, size, size)):
+            sdk[r][c] = 0
+
+        if pg.locateOnScreen('1.png', grayscale=True, confidence=0.9, region=(363+56*c, 238+56*r, size, size)):
+            sdk[r][c] = 1
+
+        elif pg.locateOnScreen('2.png', grayscale=True, confidence=0.9, region=(363+56*c, 238+56*r, size, size)):
+            sdk[r][c] = 2
+
+        elif pg.locateOnScreen('3.png', grayscale=True, confidence=0.9, region=(363+56*c, 238+56*r, size, size)):
+            sdk[r][c] = 3
+
+        elif pg.locateOnScreen('4.png', grayscale=True, confidence=0.9, region=(363+56*c, 238+56*r, size, size)):
+            sdk[r][c] = 4
+
+        elif pg.locateOnScreen('5.png', grayscale=True, confidence=0.9, region=(363+56*c, 238+56*r, size, size)):
+            sdk[r][c] = 5
+
+        elif pg.locateOnScreen('6.png', grayscale=True, confidence=0.9, region=(363+56*c, 238+56*r, size, size)):
+            sdk[r][c] = 6
+
+        elif pg.locateOnScreen('7.png', grayscale=True, confidence=0.9, region=(363+56*c, 238+56*r, size, size)):
+            sdk[r][c] = 7
+
+        elif pg.locateOnScreen('8.png', grayscale=True, confidence=0.9, region=(363+56*c, 238+56*r, size, size)):
+            sdk[r][c] = 8
+
+        elif pg.locateOnScreen('9.png', grayscale=True, confidence=0.9, region=(363+56*c, 238+56*r, size, size)):
+            sdk[r][c] = 9
+
+        else:
+            sdk[r][c] = 0
 
 def tester(row, column, num):
     
@@ -59,6 +101,21 @@ def helper():
                 return
       
     print(np.matrix(sdk))
-    input('More possible solutions')
+    
+    pg.click(390, 260)
+
+    for r in range(0,9):
+        
+        for c in range(0,9):
+            pg.press(str(sdk[r][c])) 
+            time.sleep(1)
+            pg.press('right')
+            time.sleep(1)
+
+        pg.press('down') 
+        pg.press('left', presses=8) 
 
 helper()
+
+print("Process finished --- %s seconds ---" % (time.time() - start_time))
+
